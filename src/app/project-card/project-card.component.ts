494d34c8fa777aa17815fff258154b5f96997127
Leon Lee <leonlee@LeonMBP.lan>
2024-02-17 12:47:52 -0800

    pass projectname to project-cards (child) from parent(portfolio)

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 
 @Component({
   selector: 'app-project-card',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './project-card.component.css'
 })
 export class ProjectCardComponent {
-
+  @Input() projectName: string = "";
 }
