871f2d43467ff844575bfa2ef8a4a57e7c3569b9
Leon Lee <leonlee@LeonMBP.lan>
2024-02-17 12:51:30 -0800

    create app>_model>Project.ts to keep track of project card infos

---
+++ src/app/_models/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    id: number;
+    name: string;
+    summary: string;
+    descripton: string;
+    projectLink: string;
+    pictures: string[];
+    tags: string[];
+}
