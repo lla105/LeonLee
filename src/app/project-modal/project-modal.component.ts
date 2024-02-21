bab896d44eed9cd91f62a78c7d206212aa7498a8
Leon Lee <leonlee@LeonMBP.lan>
2024-02-20 21:04:48 -0800

    project modal now works on porfolio > view more

--- src/app/project-modal/project-modal.component.ts
+++ src/app/project-modal/project-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-project-modal',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ProjectModalComponent {
 
+  constructor(public bsModalRef: BsModalRef) {
+    
+  }
 }
