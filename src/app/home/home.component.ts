48a9d91bfb3f73a245fe1e48f46e4e2326e01b3e
Leon Lee <leonlee@LeonMBP.lan>
2024-02-20 20:43:19 -0800

    also added support for home component to support featured project from projects services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { ProjectsService } from '../_services/projects.service';
+import { Project } from '../_models/Project';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  featuredProject = {} as Project;
   // set tab name
-  constructor(private titleService: Title) {
-    this.titleService.setTitle('hire me pls :(')
+  constructor(private titleService: Title, private projectService: ProjectsService) {
+    this.titleService.setTitle('Leon Lee - Home')
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.featuredProject = this.projectService.GetProjectById(1);
   }
 }
